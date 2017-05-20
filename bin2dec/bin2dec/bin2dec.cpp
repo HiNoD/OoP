32b8f83775a64bd84400f0fc73dbe30c7b50f24b
HiNoD <ink_96@mail.ru>
2017-05-20 08:32:04 +0300

    bin2dec
    Были проблемы с синхронизацией на Git

--- bin2dec/bin2dec/bin2dec.cpp
+++ bin2dec/bin2dec/bin2dec.cpp
@@ -2,12 +2,27 @@
 #include "fstream"
 #include "iostream"
 #include "string"
+<<<<<<< HEAD
+#include <math.h>
+#include <bitset>
+#include <stdlib.h>
+=======
 #include <math.h>  
+>>>>>>> origin/master
 
 using namespace std;
 
 void PrintHelp()
 {
+<<<<<<< HEAD
+	cout << "Example: bin2dec.exe 10101" << endl;
+}
+
+bool Check(const string inputNumber)
+{
+	int counter = 0;
+	while (counter < inputNumber.size())
+=======
 	cout << "Example: bin2dec.exe 10101"<< endl;
 }
 
@@ -15,18 +30,54 @@ void Check(string inputNumber, bool & error, int lengthOfString)
 {
 	int counter = 0;
 	while (counter < lengthOfString && error != true)
+>>>>>>> origin/master
 	{
 		if (inputNumber[counter] == '0' || inputNumber[counter] == '1')
 		{
 			counter++;
+<<<<<<< HEAD
+			return false;
+		}
+		else
+		{
+			return true;
+=======
 		}
 		else
 		{
 			error = true;
+>>>>>>> origin/master
 		}
 	}
 }
 
+<<<<<<< HEAD
+int Converting(const string inputNumber)
+{
+	int buffer = 2, outputNumber = 0;
+	for (int i = 0; i <= (inputNumber.size() - 1); i++)
+	{
+		if (i < (inputNumber.size() - 1))
+		{
+			buffer = buffer << (inputNumber.size()  - (i + 2));
+		}
+		else
+		{
+			buffer = 1;
+		}
+		if (inputNumber[i] == '0')
+		{
+			outputNumber += 0 * buffer;
+			buffer = 2;
+		}
+		else
+		{
+			outputNumber += 1 * buffer;
+			buffer = 2;
+		}
+	}
+	return outputNumber;
+=======
 void Converting(const string & inputNumber, int lengthOfString, double & outputNumber)
 {
 	double gadfly = 0;
@@ -63,6 +114,7 @@ void OperationRoom(char *argv[])
 		Converting(inputNumber, lengthOfString, outputNumber);
 		cout << outputNumber << endl;
 	}
+>>>>>>> origin/master
 }
 
 int main(int argc, char * argv[])
@@ -77,8 +129,30 @@ int main(int argc, char * argv[])
 	}
 	else
 	{
+<<<<<<< HEAD
+		string inputNumber = argv[1];
+
+		if (Check(inputNumber))
+		{
+			if (inputNumber.size() == 0)
+			{
+				cout << "0" << endl;
+			}
+			else
+			{
+				cout << "Wrong number" << endl;
+			}
+		}
+		else
+		{
+			cout << "Your number:" << Converting(inputNumber) << endl;
+		}
+	}
+	return 0;
+=======
 		OperationRoom(argv);
 	}
     return 0;
+>>>>>>> origin/master
 }
 
--- bin2dec/bin2dec/test.bat
+++ bin2dec/bin2dec/test.bat
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+SET program="%1"
+if %program% == "" goto err
+
+echo Test1
+bin2dec.exe %TEMP%\1111111111111111111111111111111 1111111111111111111111111111111
+if ERRORLEVEL 1 goto testFailed
+
+echo Test3
+bin2dec.exe %TEMP%\110010000 110010000
+if ERRORLEVEL 1 goto testFailed
+
+echo Test4
+bin2dec.exe %TEMP%\000 000
+if ERRORLEVEL 1 goto testFailed
+
+echo Test5
+bin2dec.exe %TEMP%\224
+if ERRORLEVEL 1 goto testFailed
+
+echo Programm testing succeeded
+exit 0
+
+:testFailed
+echo Testing failed
+exit 1;
+=======
 rem @echo off
 SET program="%1"
 if %program% == "" goto err
@@ -27,3 +54,4 @@ echo Testing failed
 
 :err
 echo Usage: test.bat <Path to program>;
+>>>>>>> origin/master
